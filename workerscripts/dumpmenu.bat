8380b6a6acb50b6003c08a434ec5418e7acc7a3a
mpax235 <millerpgraves@gmail.com>
2024-08-05 22:07:10 -0700

    Update dumpmenu.bat

--- workerscripts/dumpmenu.bat
+++ workerscripts/dumpmenu.bat
@@ -20,9 +20,30 @@ echo.
 set /p mode="Select a Option from the list with Number: "
 
 if "%mode%"=="1" (
-    echo Work in Progress. Come back soon.
+    cd mtkclient
+    if not exist "mtkclient\" (
+        echo mtkclient not found.
+	echo.
+    cd ..
+	echo Hint: Put the second mtkclient folder inside the first
+	echo for the script to read it.
+        pause
+        exit /b
+    )
+    cd mtkclient
+    python3 mtk.py dumpbrom --filename=mediatek_brom_dump_android_script.bin
 )
 
 if "%mode%"=="2" (
-    echo Work in Progress. Come back soon.
+    cd mtkclient
+    if not exist "mtkclient\" (
+        echo mtkclient not found.
+	echo.
+    cd ..
+	echo Hint: Put the second mtkclient folder inside the first
+	echo for the script to read it.
+        pause
+        exit /b
+    )
+    cd mtkclient
 )
