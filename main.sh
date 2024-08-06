a38b9ce225805ad9dab08d9be7065ce71e3bc0ec
mpax235 <millerpgraves@gmail.com>
2024-08-05 22:28:37 -0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,32 @@
+clear
+ver=1.0.1
+echo -e "\e[34mmpax235's Android All-in-One Script\e[0m"
+echo -e "\e[34mVersion $ver\e[0m"
+echo
+echo -e "\e[1m--- MAIN MENU ---\e[0m"
+echo
+echo -e "\e[1m--- ADB Devices ---\e[0m"
+adb devices
+echo
+echo -e "\e[1m--- Fastboot Devices ---\e[0m"
+fastboot devices
+echo
+echo -e "\e[1m--- MENU ---\e[0m"
+echo
+echo "(1)  Boot Menu"
+echo "(2)  Dump Menu"
+echo
+
+read -p "Select a Option from the list with Number: " mode
+
+if [ "$mode" == "1" ]; then
+    echo "Boot Menu..."
+    cd "workerscripts"
+    ./bootmenu.sh
+    cd ..
+elif [ "$mode" == "2" ]; then
+    echo "Dump Menu..."
+    cd "workerscripts"
+    ./dumpmenu.sh
+    cd ..
+fi
