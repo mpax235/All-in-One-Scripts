feadeab4d4e9e54f4f417c37579a66c5d9f57d5f
mpax235 <millerpgraves@gmail.com>
2024-08-06 07:51:01 -0700

    Create bootmenu.sh

---
+++ workerscripts/bootmenu.sh
@@ -0,0 +1,66 @@
+clear
+echo -e "\e[34mmpax235's Android All-in-One Script\e[0m"
+echo -e "\e[1m--- BOOT MENU ---\e[0m"
+echo
+echo -e "\e[1m--- ADB Devices ---\e[0m"
+adb devices
+echo
+echo -e "\e[1m--- Fastboot Devices ---\e[0m"
+fastboot devices
+echo
+echo -e "\e[1m--- MENU ---\e[0m"
+echo "- ADB STUFF -"
+echo "(1)  OS             =   Android"
+echo "(2)  Recovery       =   Android Recovery"
+echo "(3)  fastbootd      =   Android Fastboot            - ONLY FOR ANDROID 10 AND LATER -"
+echo "(4)  Fastboot       =   Fastboot"
+echo
+echo "- FASTBOOT STUFF -"
+echo "(5)  OS2            =   Android"
+echo "(6)  fastbootd2     =   Android Fastboot            - ONLY FOR ANDROID 10 AND LATER -"
+echo "(7)  Fastboot2      =   Fastboot"
+echo
+echo "- OTHER STUFF -"
+echo "(8)  Meta           =   Little Kernel Meta Mode     - ONLY FOR MEDIATEK DEVICES -"
+echo "(9)  Factory        =   Factory Mode                - ONLY FOR MEDIATEK DEVICES -"
+echo "(A)  Preloader      =   Preloader Mode              - ONLY FOR MEDIATEK DEVICES -"
+echo "(B)  Download       =   BootROM Download Mode       - ONLY FOR MEDIATEK DEVICES WITH DL ACCESS -"
+
+read -p "Select a Option from the list with Number: " mode
+
+if [ "$mode" == "1" ]; then
+    echo "Booting into Android..."
+    echo "-- IF THE SCRIPT GETS STUCK EXIT WITH CTRL+C --"
+    sudo adb reboot
+    echo "You should now boot into Android."
+elif [ "$mode" == "2" ]; then
+    echo "Booting into Android Recovery..."
+    echo "-- IF THE SCRIPT GETS STUCK EXIT WITH CTRL+C --"
+    sudo adb reboot recovery
+    echo "You should now boot into Android Recovery."
+elif [ "$mode" == "3" ]; then
+    echo "Booting into Android Fastboot..."
+    echo "-- IF THE SCRIPT GETS STUCK EXIT WITH CTRL+C --"
+    sudo adb reboot fastboot
+    echo "You should now boot into Android Fastboot."
+elif [ "$mode" == "4" ]; then
+    echo "Booting into Fastboot..."
+    echo "-- IF THE SCRIPT GETS STUCK EXIT WITH CTRL+C --"
+    sudo adb reboot bootloader
+    echo "You should now boot into Fastboot."
+elif [ "$mode" == "5" ]; then
+    echo "Booting into Android ..."
+    echo "-- IF THE SCRIPT GETS STUCK EXIT WITH CTRL+C --"
+    sudo fastboot reboot
+    echo "You should now boot into Android."
+elif [ "$mode" == "6" ]; then
+    echo "Booting into Android Fastboot..."
+    echo "-- IF THE SCRIPT GETS STUCK EXIT WITH CTRL+C --"
+    sudo fastboot reboot fastboot
+    echo "You should now boot into Android Fastboot."
+elif [ "$mode" == "7" ]; then
+    echo "Booting into Fastboot..."
+    echo "-- IF THE SCRIPT GETS STUCK EXIT WITH CTRL+C --"
+    sudo fastboot reboot bootloader
+    echo "You should now boot into Fastboot."
+fi
