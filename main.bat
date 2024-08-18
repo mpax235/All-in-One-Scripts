210a74793143cfdc9de4cead246c2e4f0ff712b1
mpax235 <millerpgraves@gmail.com>
2024-08-18 08:56:56 -0700

    Update main.bat

--- main.bat
+++ main.bat
@@ -36,6 +36,7 @@ echo *** MENU ***
 echo.
 echo (1)  Boot Menu
 echo (2)  Dump Menu
+echo (3)  Device Menu
 echo.
 
 set /p mode="Select a Option from the list with Number: "
@@ -51,5 +52,12 @@ if "%mode%"=="2" (
     echo Dump Menu...
     cd "workerscripts"
     dumpmenu.bat
+    cd ..
+)
+
+if "%mode%"=="3" (
+    echo Device Menu...
+    cd "workerscripts"
+    devicemenu.bat
     cd ..
 )
