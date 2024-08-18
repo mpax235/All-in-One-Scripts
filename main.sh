6e939c56832ca7bb26432178fbdab348b6c9342b
mpax235 <millerpgraves@gmail.com>
2024-08-18 08:57:44 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -17,6 +17,7 @@ echo -e "\e[1m--- MENU ---\e[0m"
 echo
 echo "(1)  Boot Menu"
 echo "(2)  Dump Menu"
+echo "(3)  Device Menu"
 echo
 
 read -p "Select a Option from the list with Number: " mode
@@ -30,5 +31,10 @@ elif [ "$mode" == "2" ]; then
     echo "Dump Menu..."
     cd "workerscripts"
     ./dumpmenu.sh
+    cd ..
+elif [ "$mode" == "3" ]; then
+    echo "Device Menu..."
+    cd "workerscripts"
+    ./devicemenu.sh
     cd ..
 fi
