42b9ad5747fa2bbada44c9fbfdd4b4285c40c8de
mpax235 <millerpgraves@gmail.com>
2024-08-18 08:58:32 -0700

    Update dumpmenu.bat

--- workerscripts/dumpmenu.bat
+++ workerscripts/dumpmenu.bat
@@ -14,7 +14,6 @@ echo *** MENU ***
 echo.
 echo * MEDIATEK STUFF *
 echo (1)  brom           =   Dump the SoC's BootROM
-echo (2)  preloader      =   Dump the Preloader
 echo.
 
 set /p mode="Select a Option from the list with Number: "
@@ -37,23 +36,4 @@ if "%mode%"=="1" (
     color 7
     cls
     python3 mtk.py dumpbrom --filename=mediatek_brom_dump_android_script.bin
-)
-
-if "%mode%"=="2" (
-    cd mtkclient
-    if not exist "mtkclient\" (
-        echo mtkclient not found.
-	echo.
-    cd ..
-	echo Hint: Put the second mtkclient folder (if zip) inside the first
-	echo for the script to read it.
-	echo.
-	echo or if you git cloned mtkclient, just put the mtkclient folder
-	echo in the mtkclient folder in workerscripts for the script to read it.
-        pause
-        exit /b
-    )
-    cd mtkclient
-    color 7
-    cls
 )
