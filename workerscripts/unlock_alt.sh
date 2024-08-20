78d39b3c69b4e7a0f5a5d4abb620c889f044c73d
mpax235 <millerpgraves@gmail.com>
2024-08-19 21:55:30 -0700

    Create unlock_alt.sh

---
+++ workerscripts/unlock_alt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "\e[34mSelect which exploit to use. (MAKE SURE TO USE THE CORRECT ONE FOR YOUR DEVICE)\e[0m"
+echo
+echo -e "\e[93m--- AMAZON ---"
+echo -e "amonet-austin                                          'for the Fire 7 (2017)'"
+echo -e "amonet-ariel                                           'for the Fire HD 6/7 (2014)'"
+echo -e "amonet-karnak                                          'for the Fire HD 8 (2018)'"
+echo -e "amonet-mustang                                         'for the Fire 7 (2019)'"
+echo -e "amonet-thempis                                         'for the Fire HD 8/10 (2015)'"
+echo -e "amonet-douglas                                         'for the Fire HD 8 (2017)'"
+echo -e "amonet-suez                                            'for the Fire HD 10 (2017)'"
+echo -e "amonet-ford                                            'for the Fire 7 (2015)'"
+echo -e "amonet-tank                                            'for the Fire TV Stick (2016-2019)'"
+echo -e "amonet-biscuit                                         'for the Echo Dot 2 (2016)'"
+echo -e "amonet-giza                                            'for the Fire HD 8 (2016)'"
+echo -e "kamakiri-mantis                                        'for the Fire TV Stick 4K (2018)'"
+echo -e "kamakiri-sheldon                                       'for the Fire TV Stick and Lite (2020)'"
