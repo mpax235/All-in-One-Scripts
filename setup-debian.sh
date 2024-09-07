b8c50a1be4e773c234f1ab00125bc725fbf8e6ed
mpax235 <millerpgraves@gmail.com>
2024-09-06 21:02:31 -0700

    Update setup-debian.sh

--- setup-debian.sh
+++ setup-debian.sh
@@ -1,7 +1,7 @@
 echo -e "This will setup the Android All-in-One Script for use."
 echo -e "This will also disable SELinux temporarily to make sure nothing goes wrong, and then re-enable it after."
 echo -e "SELinux disabling does not happen if SELinux is entirely disabled."
-echo -e ""
+echo
 read -p "Press any key to continue... " setup
 
 # disable SElinux temporarily
